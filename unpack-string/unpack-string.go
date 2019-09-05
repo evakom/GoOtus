25cefd50bb1755e730e02ef17008028c35f20fb9
Eugene Klimov <tirava@gmail.com>
2019-09-05 22:07:09 +0300

    hw-2 start

--- unpack-string/unpack-string.go
+++ unpack-string/unpack-string.go
@@ -1,8 +1,8 @@
 /*
- * HomeWork-x: xxx
+ * HomeWork-2: Unpack String
  * Created on 05.09.19 22:04
  * Copyright (c) 2019 - Eugene Klimov
  */
 
-// Package unpack_string implements ... .
-package unpack_string
+// Package unpackstring implements ... .
+package unpackstring
