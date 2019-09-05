c786dd2f618d39edf4a4e374e705f124db8cccf8
Eugene Klimov <tirava@gmail.com>
2019-09-05 23:21:00 +0300

    hw-2 draft

--- unpack-string/unpack-string.go
+++ unpack-string/unpack-string.go
@@ -4,5 +4,36 @@
  * Copyright (c) 2019 - Eugene Klimov
  */
 
-// Package unpackstring implements ... .
+// Package unpackstring implements unpacking string.
 package unpackstring
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// RunLengthDecode returns RLE decoded.
+func RunLengthDecode(input string) string {
+	result := strings.Builder{}
+	digits := ""
+
+	for _, r := range input {
+		if unicode.IsDigit(r) {
+			digits += string(r)
+			continue
+		}
+
+		digit, err := strconv.Atoi(digits)
+		if err != nil {
+			result.WriteRune(r)
+			continue
+		}
+
+		result.WriteString(strings.Repeat(string(r), digit))
+
+		digits = ""
+	}
+
+	return result.String()
+}
