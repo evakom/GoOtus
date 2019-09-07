655ba361e2cf8582441408a8b3a18169ee097b86
Eugene Klimov <tirava@gmail.com>
2019-09-07 13:49:45 +0300

    hw-2 draft logic

--- unpack-string/unpack-string.go
+++ unpack-string/unpack-string.go
@@ -15,10 +15,32 @@ import (
 
 // UnpackString returns string unpacked.
 func UnpackString(input string) string {
-	result := strings.Builder{}
+
+	result := strings.Builder{} // faster then string +=
 	digits := ""
+	char := ""
 
 	for _, r := range input {
+
+		//if !unicode.IsLetter(r) && digits == "" { // fail string
+		//	println("---------fail-----------", input, string(r), result.String())
+		//	return ""
+		//}
+
+		if unicode.IsLetter(r) && char == "" { // 1st char
+			char = string(r)
+			result.WriteString(char)
+			//println("---------1st char-----------", input, char, result.String())
+			continue
+		}
+
+		if unicode.IsLetter(r) && char != "" { // char after char
+			char = string(r)
+			result.WriteString(char)
+			//println("---------char char-----------", input, char, result.String())
+			continue
+		}
+
 		if unicode.IsDigit(r) {
 			digits += string(r)
 			continue
@@ -26,13 +48,15 @@ func UnpackString(input string) string {
 
 		digit, err := strconv.Atoi(digits)
 		if err != nil {
+			//println("---------------------")
 			result.WriteRune(r)
 			continue
 		}
 
-		result.WriteString(strings.Repeat(string(r), digit))
+		result.WriteString(strings.Repeat(string(char), digit))
 
 		digits = ""
+		char = ""
 	}
 
 	return result.String()
