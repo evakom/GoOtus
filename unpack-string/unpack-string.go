6940ba2e7c5da79cddfa7ad51b1b5b6e090ce289
Eugene Klimov <tirava@gmail.com>
2019-09-07 22:28:41 +0300

    hw-2 draft logic - 2

--- unpack-string/unpack-string.go
+++ unpack-string/unpack-string.go
@@ -16,65 +16,53 @@ import (
 // UnpackString returns string unpacked.
 func UnpackString(input string) string {
 
-	result := strings.Builder{}
+	//result := strings.Builder{}
+	result := ""
 	digits := ""
 	char := ""
 
-	for _, r := range input {
+	for i, r := range input {
 
-		//if !unicode.IsLetter(r) && digits == "" { // fail string
-		//	println("---------fail-----------", input, string(r), result.String())
-		//	return ""
-		//}
-		digit, _ := strconv.Atoi(digits) // no need to check error - IsDigit below check it
-		//println(char, digit, digits)
-		if digit > 0 { // unpack series
-			//digit, _ := strconv.Atoi(digits) // no need to check error - IsDigit below check it
-			//if err != nil { // write one char only if digit unknown - need tests?
-			//	println("----------fail digit-----------", digit)
-			//	result.WriteString(char)
-			//	continue
-			//}
-			//println(char, digit)
-			//if digit != 0 { // one char already appended
-			//	digit--
-			//}
-			result.WriteString(strings.Repeat(char, digit-1))
-			digits = ""
-			char = ""
-		}
-
-		if unicode.IsLetter(r) { //} && char == "" { // 1st char
-			char = string(r)
-			result.WriteString(char)
-			//println("---------1st char-----------", input, char, result.String())
-			continue
-		}
-
-		//if unicode.IsLetter(r) && char != "" { // char after char
+		//if unicode.IsLetter(r) || unicode.IsSpace(r) {
 		//	char = string(r)
-		//	result.WriteString(char)
-		//	//println("---------char char-----------", input, char, result.String())
+		//	//digits = "1"
+		//	//result.WriteString(char)
 		//	continue
 		//}
 
 		if unicode.IsDigit(r) {
 			digits += string(r)
-			continue
+			if i != len(input)-1 { // last char is number
+				continue
+			}
 		}
 
-		//digit, err := strconv.Atoi(digits)
-		//if err != nil {
-		//	//println("---------------------")
-		//	result.WriteRune(r)
-		//	continue
-		//}
+		digit, _ := strconv.Atoi(digits) // no need to check error - IsDigit checks it
+		if digit == 0 && len(digits) > 0 {
+			//println(input, char, digits, digit)
+			result = result[:len(result)-1]
+			char = ""
+			//digits = ""
+			//continue
+		}
 
-		//result.WriteString(strings.Repeat(string(char), digit))
+		if digit > 0 {
+			//println("in digit", input, char, digit, digits)
+			//result.WriteString(strings.Repeat(char, digit-1)) // one char already appended
+			result += strings.Repeat(char, digit-1) // one char already appended
+			digits = ""
+			char = ""
+		}
 
-		//digits = ""
-		//char = ""
+		if unicode.IsLetter(r) || unicode.IsSpace(r) || unicode.IsPunct(r) || unicode.IsSymbol(r) {
+			char = string(r)
+			//digits = "1"
+			//result.WriteString(char)
+			result += char
+			//continue
+		}
 	}
 
-	return result.String()
+	//return result.String()
+	return result
 }
