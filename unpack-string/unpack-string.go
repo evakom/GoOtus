b5492b299c1905b6550ad2bc9d2cc71110aba730
Eugene Klimov <tirava@gmail.com>
2019-09-07 20:03:28 +0300

    hw-2 draft logic - 1

--- unpack-string/unpack-string.go
+++ unpack-string/unpack-string.go
@@ -16,7 +16,7 @@ import (
 // UnpackString returns string unpacked.
 func UnpackString(input string) string {
 
-	result := strings.Builder{} // faster then string +=
+	result := strings.Builder{}
 	digits := ""
 	char := ""
 
@@ -26,37 +26,54 @@ func UnpackString(input string) string {
 		//	println("---------fail-----------", input, string(r), result.String())
 		//	return ""
 		//}
+		digit, _ := strconv.Atoi(digits) // no need to check error - IsDigit below check it
+		//println(char, digit, digits)
+		if digit > 0 { // unpack series
+			//digit, _ := strconv.Atoi(digits) // no need to check error - IsDigit below check it
+			//if err != nil { // write one char only if digit unknown - need tests?
+			//	println("----------fail digit-----------", digit)
+			//	result.WriteString(char)
+			//	continue
+			//}
+			//println(char, digit)
+			//if digit != 0 { // one char already appended
+			//	digit--
+			//}
+			result.WriteString(strings.Repeat(char, digit-1))
+			digits = ""
+			char = ""
+		}
 
-		if unicode.IsLetter(r) && char == "" { // 1st char
+		if unicode.IsLetter(r) { //} && char == "" { // 1st char
 			char = string(r)
 			result.WriteString(char)
 			//println("---------1st char-----------", input, char, result.String())
 			continue
 		}
 
-		if unicode.IsLetter(r) && char != "" { // char after char
-			char = string(r)
-			result.WriteString(char)
-			//println("---------char char-----------", input, char, result.String())
-			continue
-		}
+		//if unicode.IsLetter(r) && char != "" { // char after char
+		//	char = string(r)
+		//	result.WriteString(char)
+		//	//println("---------char char-----------", input, char, result.String())
+		//	continue
+		//}
 
 		if unicode.IsDigit(r) {
 			digits += string(r)
 			continue
 		}
 
-		digit, err := strconv.Atoi(digits)
-		if err != nil {
-			//println("---------------------")
-			result.WriteRune(r)
-			continue
-		}
+		//digit, err := strconv.Atoi(digits)
+		//if err != nil {
+		//	//println("---------------------")
+		//	result.WriteRune(r)
+		//	continue
+		//}
 
-		result.WriteString(strings.Repeat(string(char), digit))
+		//result.WriteString(strings.Repeat(string(char), digit))
 
-		digits = ""
-		char = ""
+		//digits = ""
+		//char = ""
 	}
 
 	return result.String()
