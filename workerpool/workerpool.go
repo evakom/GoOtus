94fe96b40f45090b1dd21c873be685b68ec2e52c
Eugene Klimov <tirava@gmail.com>
2019-09-28 12:30:30 +0300

    WorkerPool - very draft work

--- workerpool/workerpool.go
+++ workerpool/workerpool.go
@@ -9,41 +9,75 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/sync/errgroup"
+	"log"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
-const jobTimeOut = 5 // in seconds
+const (
+	JOBTIMEOUT = 5  // in seconds
+	JOBSNUM    = 10 // number of all jobs
+	MAXJOBS    = 3  // max concurrency jobs
+	MAXERRORS  = 2  // max errors from all jobs
+)
 
 type Job func() error
 
-// WorkerPool is the job worker.
-func WorkerPool(jobs []Job, maxJobs int, maxErrors int) {
-	for _, job := range jobs {
-		fmt.Println(job())
+// WorkerPool is the main worker pool manager.
+func WorkerPool(jobs []Job, maxJobs int, maxErrors int) error {
+	var eg errgroup.Group
+	jobsChan := make(chan Job, MAXJOBS)
+
+	// start workers
+	for i := 0; i < MAXJOBS; i++ {
+		i := i
+		eg.Go(func() error {
+			return jobWorker(i, jobsChan)
+		})
 	}
-	//jobControl := make(chan struct{})
-	//for i, job := range jobs {
-	//	go startJob(j, jobControl)
-	//}
+
+	// send jobs to workers
+	for _, j := range jobs {
+		jobsChan <- j
+	}
+	close(jobsChan)
+
+	return eg.Wait()
+}
+
+func jobWorker(workerNum int, inJob <-chan Job) error {
+	for j := range inJob {
+		fmt.Printf("Worker: %d started\n", workerNum)
+		fmt.Println(j())
+		fmt.Printf("Worker: %d finished\n", workerNum)
+		runtime.Gosched() // common go
+	}
+	return nil
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	jobs := make([]Job, 0)
 
-	for i := 0; i < 11; i++ {
-		i := i // for correct parameter in func
+	// jobs slice
+	for i := 0; i < JOBSNUM; i++ {
 		job := func() error {
-			time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
-			if rand.Intn(2) == 0 { // error gen
-				return fmt.Errorf("job '%d' ended with error", i)
+			d := rand.Intn(JOBSNUM)                    // random time for every job
+			time.Sleep(time.Duration(d) * time.Second) // any work here
+			if rand.Intn(2) == 0 {                     // error gen randomly
+				return fmt.Errorf("job ended with error")
 			}
-			fmt.Printf("job '%d' ended successfully", i)
+			fmt.Printf("job ended successfully, duration: %d\n", d)
 			return nil
 		}
 		jobs = append(jobs, job)
 	}
 
-	WorkerPool(jobs, 3, 2)
+	// start
+	if err := WorkerPool(jobs, MAXJOBS, MAXERRORS); err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("All jobs returned successfully!")
 }
