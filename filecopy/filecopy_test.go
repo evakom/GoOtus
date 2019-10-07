02663109828c7e2c70f6127352038e568307a63c
Eugene Klimov <tirava@gmail.com>
2019-10-07 18:43:45 +0300

    FileCopy - first test

--- filecopy/filecopy_test.go
+++ filecopy/filecopy_test.go
@@ -5,3 +5,47 @@
  */
 
 package main
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var testCases = []struct {
+	writer           io.Writer
+	fromFile, toFile string
+	offset, limit    int64
+	expError         bool
+	description      string
+}{
+	{
+		ioutil.Discard,
+		"/var/log/auth.log",
+		"test.log",
+		0, 0,
+		false,
+		"simple copy all file with no limit and offset",
+	},
+}
+
+func TestCopyFileSeekLimit(t *testing.T) {
+	for _, test := range testCases {
+		from, _ := os.Open(test.fromFile)
+		stat, _ := from.Stat()
+		expectedBytes := stat.Size()
+		actualBytes, err := CopyFileSeekLimit(test.writer, test.toFile, test.fromFile, test.offset, test.limit)
+		if err != nil && !test.expError {
+			t.Errorf("FAIL %s - CopyFileSeekLimit() returns error = '%s', expected = 'no error'.",
+				test.description, err)
+			continue
+		}
+		if expectedBytes != actualBytes {
+			t.Errorf("FAIL %s - CopyFileSeekLimit() returns bytes = '%d', expected bytes = '%d'.",
+				test.description, actualBytes, expectedBytes)
+			continue
+		}
+		t.Logf("PASS CopyFileSeekLimit - %s", test.description)
+	}
+}
