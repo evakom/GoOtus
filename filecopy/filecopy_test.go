36b287f43c47ab357d46bd0538e4871b91a5e037
Eugene Klimov <tirava@gmail.com>
2019-10-07 21:44:23 +0300

    FileCopy - second test

--- filecopy/filecopy_test.go
+++ filecopy/filecopy_test.go
@@ -28,19 +28,50 @@ var testCases = []struct {
 		false,
 		"simple copy all file with no limit and offset",
 	},
+	{
+		ioutil.Discard,
+		"fake.fak",
+		"test.log",
+		0, 0,
+		true,
+		"error open source file",
+	},
+	{
+		ioutil.Discard,
+		"/var/log/auth.log",
+		"",
+		0, 0,
+		true,
+		"error create destination file",
+	},
 }
 
 func TestCopyFileSeekLimit(t *testing.T) {
 	for _, test := range testCases {
-		from, _ := os.Open(test.fromFile)
-		stat, _ := from.Stat()
-		expectedBytes := stat.Size()
 		actualBytes, err := CopyFileSeekLimit(test.writer, test.toFile, test.fromFile, test.offset, test.limit)
-		if err != nil && !test.expError {
-			t.Errorf("FAIL %s - CopyFileSeekLimit() returns error = '%s', expected = 'no error'.",
+		if err != nil {
+			if !test.expError {
+				t.Errorf("FAIL %s - CopyFileSeekLimit() returns error = '%s', expected = 'no error'.",
+					test.description, err)
+			} else {
+				t.Logf("PASS CopyFileSeekLimit - %s", test.description)
+			}
+			continue
+		}
+		if test.expError {
+			t.Errorf("FAIL %s - CopyFileSeekLimit() returns = 'no error', expected error = '%s'.",
 				test.description, err)
 			continue
 		}
+		from, err := os.Open(test.fromFile)
+		if err != nil {
+			t.Fatalf("can't open test file: %s", err)
+		}
+		stat, err := from.Stat()
+		if err != nil {
+			t.Fatalf("can't get file stat: %s", err)
+		}
+		expectedBytes := stat.Size()
 		if expectedBytes != actualBytes {
 			t.Errorf("FAIL %s - CopyFileSeekLimit() returns bytes = '%d', expected bytes = '%d'.",
 				test.description, actualBytes, expectedBytes)
